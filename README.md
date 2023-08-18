6012db664c0ab3918a56dec59f452d8c70f64848
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-18 11:43:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Array-Ex-2-Minimum-Maximum-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-neb72h)
---
+++ index.js
@@ -0,0 +1,15 @@
+let numbers = [76,23,78,87,99,121,9,8]
+
+let maxNumber = numbers[0];
+let miniNumber = numbers[0];
+
+for(let i=1; i<numbers.length; i++){
+    if(numbers[i]>maxNumber){
+        maxNumber = numbers[i] ;
+}
+    if(numbers[i]<miniNumber){
+        numbers[i] = miniNumber;
+    }
+}
+console.log('Maximum Number is :' + maxNumber);
+console.log('Minimum Number is:' + miniNumber);
